23eb8c8def36c920dd577a4b0806fcb42a551626
stirjanf <filip.stirjan@outlook.com>
2025-02-07 12:27:03 +0100

    v8

--- flashcards.py
+++ flashcards.py
@@ -2,11 +2,16 @@ import pandas as pd
 from colorama import Fore, Style
 import random
 import os
+import tempfile
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
 
 def select(sheets):
     os.system('cls')
     while True:
-        n = input(f"How many words do you want to practice? > ").strip()
+        n = input(f"How many words do you want to practice? (x = all)> ").strip()
         if n == "q":
             exit()
         if n:
@@ -14,29 +19,49 @@ def select(sheets):
     os.system('cls')
     for sheet,i in zip(sheets,range(len(sheets))):
         print(f"[{i+1}] - {sheet}")
-    print(f"[Q] - Quit")
+    print(f"[5] - All\n[Q] - Quit")
     while True:
         opt = input(f"What do you want to practice? > ").strip()
         if opt == "q":
             exit()
+        if opt and n == "x":
+            return n,int(opt)
         if opt:
             return int(n),int(opt)
+
 
 def play(obj):
     os.system('cls')
     unused = obj
     n = len(obj)
     used = []
+    to_learn = []
     while unused:
         entry = random.choice(unused)
         used.append(entry)
         inpt = input(f"\n{Fore.GREEN}Riječ {len(used)}/{n}: {Fore.BLUE}{entry[-1]} {Style.RESET_ALL}> ")
         if inpt == "q":
             break    
+        if inpt != "da":
+            to_learn.append(entry)
         strng = " - ".join(map(str, entry[1:-1])) 
         print(f"{Fore.YELLOW}{strng}{Style.RESET_ALL}")
         unused.remove(entry)
         if not unused:
+            pdfmetrics.registerFont(TTFont('Arial', 'Arial.ttf'))
+            score = n-len(to_learn)
+            print(f"{Fore.YELLOW}Score: {score}/{n} ({(score/n)*100})%{Style.RESET_ALL}")
+            with tempfile.NamedTemporaryFile(delete=False, suffix='.pdf') as tmp:
+                c = canvas.Canvas(tmp.name, pagesize=letter)
+                c.setFont("Arial", 12)
+                y = 750
+                for item in to_learn:
+                    item = str(item)
+                    c.drawString(100,y,item)
+                    y -= 20
+                c.save()
+            os.startfile(tmp.name)
+            input(f"Press enter to leave...")
             break
 
 data = []
@@ -62,8 +87,15 @@ while True:
     nwords,opt = select(sheets)
     if opt == 5:
         words = []
-        for i in range(len(data)):
-            words.append((data[i])[:nwords])
+        if nwords == "x":
+            for i in range(len(data)):
+                words.append((data[i]))
+        else: 
+            for i in range(len(data)):
+                words.append((data[i])[:nwords])
         play([item for sublist in words for item in sublist])
     else:
-        play(data[opt-1][:nwords])
+        if nwords == "x":
+            play(data[opt-1])
+        else:
+            play(data[opt-1][:nwords])
